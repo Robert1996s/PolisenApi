dd2d425735bd08d590c1595994c34154ad646c63
Robert1996s <54981029+Robert1996s@users.noreply.github.com>
2020-09-15 10:45:52 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const url = 'https://polisen.se/api/events?locationname=Stockholm;';
+const categoriesToDisplay = ['Date', 'Type', 'Location'];
+
+    function getData(asset = 'Stockholm') {
+         return fetch(url)
+        .then(response => response.json())
+        .then(response => renderTable(response))
+        }
+    
+
+
+
+
+    function onClick() {
+        let asset = document.getElementById("textInputCategory").value;
+        
+        getData(asset)
+        
+
+        }
+
+        
+        
+
+    function renderTable(response) {
+        var tableBody = document.querySelector("#crimeTable");
+        var crimes = response;
+        tableBody.innerHTML = "";
+        console.log(response);
+        
+
+        var date = document.createElement("tr");
+    
+    
+
+
+        crimes.forEach(crime => {
+            let tableRow = document.createElement("tr");
+            let crimeTypeDate = document.createElement("td");
+            crimeTypeDate.innerHTML = new Date(crime.datetime).toDateString();
+
+            let crimeTypeId = document.createElement("td");
+            crimeTypeId.innerHTML = crime.type;
+
+            let crimeTypeLocation = document.createElement("td");
+            crimeTypeLocation.innerHTML = crime.location.name
+
+            tableRow.appendChild(crimeTypeId);
+            tableRow.appendChild(crimeTypeDate);
+            tableRow.appendChild(crimeTypeLocation);
+            tableBody.appendChild(tableRow);
+
+            });
+        
+        }
+
+        
+renderTable(response);
